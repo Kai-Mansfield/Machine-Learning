9d937436cfdc050170a61374b5839c82a8997051
KaiM99 <50274436+KaiM99@users.noreply.github.com>
2020-03-20 20:53:29 +0000

    Add files via upload

---
+++ gym.py
@@ -0,0 +1,11 @@
+Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import gym
+
+Warning (from warnings module):
+  File "C:\Python\Python36\lib\site-packages\numpy\_distributor_init.py", line 32
+    stacklevel=1)
+UserWarning: loaded more than 1 DLL from .libs:
+C:\Python\Python36\lib\site-packages\numpy\.libs\libopenblas.IPBC74C7KURV7CB2PKT5Z5FNR3SIBV4J.gfortran-win_amd64.dll
+C:\Python\Python36\lib\site-packages\numpy\.libs\libopenblas.PYQHXLVVQ7VESDPUVUADXEVJOBGHJPAY.gfortran-win_amd64.dll
+>>> 
